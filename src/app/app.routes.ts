7613c88100cd733f5853fbdc17d7516b08abfef2
Seif <dr.saifos2001@gmail.com>
2025-06-22 17:51:43 +0300

    nabar done and routing done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { Portfolio } from './portfolio/portfolio';
+import { NotFound } from './not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: Home, title: 'Home' },
+  { path: 'about', component: About, title: 'About' },
+  { path: 'contact', component: Contact, title: 'Contact Us' },
+  { path: 'portfolio', component: Portfolio, title: 'Portfolio' },
+  { path: '**', component: NotFound, title: 'Not Found' },
+];
